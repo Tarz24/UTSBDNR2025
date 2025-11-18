fe4745645432c62ef8c9370d7f54d23d2b31d682
Kasyful Haq B. <82114912+DatBoiSUS-Baka@users.noreply.github.com>
2025-11-18 14:43:12 +0700

    feat: implementasi logika tambah jadwal baru

--- backend/src/controllers/jadwalController.js
+++ backend/src/controllers/jadwalController.js
@@ -2,24 +2,51 @@
 // Kasyful Haq Bachariputra
 // 17 November 2025
 
-const model = require('../models/Jadwal');
+const { body, validationResult } = require('express-validator');
+const JadwalModel = require('../models/Jadwal');
 
 const getJadwal = async (req, res) => {
-    // 1. Logic to extract data from req (e.g., req.query)
+    // 1. Logic to extract data from req
     const rute_awal = req.query.rute_awal;
     const rute_tujuan = req.query.rute_tujuan;
     const jam_berangkat = req.query.jam_berangkat;
 
-    // 2. Logic to talk to the database (e.g., Model.find())
-    const jadwal = await model.find({
+    // 2. Logic to talk to the database
+    const jadwal = await JadwalModel.find({
         rute_awal: rute_awal,
         rute_tujuan: rute_tujuan,
         jam_berangkat: jam_berangkat
     });
 
-    // 3. Logic to send the response (e.g., res.json() or res.status())
+    // 3. Logic to send the response
     res.json(jadwal);
 };
 
-module.exports = { getJadwal };
+const createJadwal = async (req, res) => {
+    // Ambil hasil validasi dari request yang masuk
+    const result = validationResult(req);
+
+    // bila tidak error, lakukan instansiasi model dan penyimpanan ke database
+    if (result.isEmpty()){
+        try{
+            const newJadwalDoc = new JadwalModel(req.body);
+
+            const savedJadwal = await newJadwalDoc.save();
+
+            res.status(201).json(savedJadwal);
+        }catch (error) {
+            console.error("Error membuat jadwal:", error);
+            res.status(500).json({
+                message: "Internal server error, tidak bisa save jadwal",
+                details: error.message
+            });
+        }
+    }
+    // apabila error, kirim status yang sesuai
+    else{
+        res.status(400).send({ errors: result.array() });
+    }
+}
+
+module.exports = { getJadwal, createJadwal };
 
--- backend/src/routes/jadwalRoutes.js
+++ backend/src/routes/jadwalRoutes.js
@@ -2,12 +2,19 @@
 // Kasyful Haq Bachariputra
 // 17 November 2025
 
+const { body } = require('express-validator');
 const express = require('express');
 const router = require('express').Router();
 
-const { getJadwal } = require('../controllers/jadwalController');
+const { getJadwal, createJadwal } = require('../controllers/jadwalController');
 
 // Route handler untuk model 'jadwal'
+// get
 router.get('/jadwal', getJadwal);
+
+// post
+router.post('/jadwal', body('rute_awal').notEmpty(), body('rute_tujuan').notEmpty(), body('jam_berangkat').isDate().notEmpty(), 
+body('pool_keberangkatan').notEmpty(), body('pool_tujuan').notEmpty(), body('estimasi_jam_tiba').isDate().notEmpty(), body('harga').isInt().notEmpty(),
+body('total_kursi').isInt().notEmpty(), body('kursi_tersedia').isInt().notEmpty(), createJadwal);
 
 module.exports = router;
